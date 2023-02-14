736fbdb4fe11887bcfe163f56526f80fa96b90e7
danielstoller <danhstoller@gmail.com>
2023-02-13 20:17:54 -0500

    Nodes pushed

--- Node.hpp
+++ Node.hpp
@@ -3,3 +3,27 @@
 
 //A Node is a spot on the maze where the car needs to make a decision that isn't just forward 
 //and is given an option of a left and/or right turn
+
+
+#ifndef NODE_
+#define NODE_
+
+template<class ItemType>
+class Node
+{
+private:
+   ItemType        item; // A data item
+   Node<ItemType>* next; // Pointer to next node
+   
+public:
+   Node();
+   Node(const ItemType& anItem);
+   Node(const ItemType& anItem, Node<ItemType>* nextNodePtr);
+   void setItem(const ItemType& anItem);
+   void setNext(Node<ItemType>* nextNodePtr);
+   ItemType getItem() const ;
+   Node<ItemType>* getNext() const ;
+}; // end Node
+
+#include "Node.tpp"
+#endif
