3db24b21dde8d23d7c62a1817596abb4d0a64f0e
danielstoller <danhstoller@gmail.com>
2023-02-27 19:56:18 -0500

    Need to add a linked list class

--- Node.cpp
+++ Node.cpp
@@ -1,70 +1,108 @@
 #include "Node.hpp"
+#include <string>
 
-Node::Node() : next(nullptr)
-{
-} // end default constructor
 
-Node::Node(const int &X, const int &Y) : x(X), y(Y), next(nullptr)
+//Default Node Constructor
+//PURPOSE: This will be called when we first start 
+Node::Node() : next1(nullptr), next2(nullptr), next3(nullptr), next4(nullptr)
 {
-} // end constructor
+   //Fill the coordinate arrays with 0's
+   for(int i=0; i<144; i++){
+      X[i] = 0;
+      Y[i] = 0;
+   }
 
-Node::Node(const ItemType &anItem, Node<ItemType> *nextNodePtr) : item(anItem), next(nextNodePtr)
-{
-} // end constructor
+   //Update total
+   total++;
+   //Initialize node index
+   index = total;
 
-void Node::setCoordinate(const ItemType &anItem) // Change this to either 2 ints (x, y) or a 2d array
+   //Go forward 1 square
+   //**Might need to make it call a function that goes until it hits a wall then centers itself instead**
+}
+
+Node::Node(const int &x, const int &y) : next1(nullptr), next2(nullptr), next3(nullptr), next4(nullptr)
 {
-   item = anItem;
+   //Update total
+   total++;
+   //Initialize node index
+   index = total;
+   //Update the coordinate array with the new coordinate
+   X[total] = x;
+   Y[total] = y;
+}
+
+Node::Node(const int &x, const int &y, Node* nextNodePtr) : next1(nullptr), next2(nullptr), next3(nullptr), next4(nullptr)
+{
+   //Update total
+   total++;
+   //Initialize node index
+   index = total;
+   //Update the coordinate array with the new coordinate
+   X[total] = x;
+   Y[total] = y;
+}
+
+
+void Node::setCoordinate(const int &x, const int &y, const int &i)  //Change this to either 2 ints (x, y) or a 2d array
+{
+   X[i] = x;
+   Y[i] = y;
 } // end setItem
 
-void Node::setNext1(Node *nextNodePtr)
+int Node::getXCoordinate(int i) const
 {
-   next = nextNodePtr;
-} // end setNext
-
-template <class ItemType>
-void Node::setNext2(Node *nextNodePtr)
-{
-   next = nextNodePtr;
-} // end setNext
-
-template <class ItemType>
-void Node::setNext3(Node *nextNodePtr)
-{
-   next = nextNodePtr;
-} // end setNext
-
-void Node::setNext4(Node *nextNodePtr)
-{
-   next = nextNodePtr;
-} // end setNext
-
-int Node::getXCoordinate() const
-{
-   return xCoor[nodeIndex];
+   return X[i];
 }
 
-int Node::getYCoordinate() const
+int Node::getYCoordinate(int i) const
 {
-   return yCoor[nodeIndex];
+   return Y[i];
 }
 
-Node *Node::getNext1() const
+void Node::setNext1(Node* nextNodePtr)
 {
-   return next;
+   next1 = nextNodePtr;
+} // end setNext
+
+void Node::setNext2(Node* nextNodePtr)
+{
+   next2 = nextNodePtr;
+} // end setNext
+
+void Node::setNext3(Node* nextNodePtr)
+{
+   next3 = nextNodePtr;
+} // end setNext
+
+
+void Node::setNext4(Node* nextNodePtr)
+{
+   next4 = nextNodePtr;
+} // end setNext
+
+
+Node* Node::getNext1() const
+{
+   return next1;
 } // end getNext
 
-Node *Node::getNext2() const
+
+Node* Node::getNext2() const
 {
-   return next;
+   return next2;
 } // end getNext
 
-Node *Node::getNext3() const
+Node* Node::getNext3() const
 {
-   return next;
+   return next3;
 } // end getNext
 
-Node *Node::getNext4() const
+
+Node* Node::getNext4() const
 {
-   return next;
+   return next4;
 } // end getNext
+
+
+int Node::total = 0;
