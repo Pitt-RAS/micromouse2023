a834c4edf78da365d281956bde86a6e7c6bbfd53
CBSchiffer <122397810+CBSchiffer@users.noreply.github.com>
2023-10-31 15:34:26 -0400

    Updated simulation files
    Simulation of maze is now being drawn properly.
    Bot is also drawn properly now

--- sim/main.py
+++ sim/main.py
@@ -2,28 +2,51 @@
 from tkinter import *
 from numpy import *
 from maze import Maze
+from bot import Bot
+from node import Node
 
-tileSize = 50
+tileSize = 35
+fontSize = int(tileSize / 5)
+labels = True
 
 if __name__ == '__main__':
     root = Tk()
     root.title("Generic Window")
-    root.geometry('1920x1080')
+    root.geometry('1536x864')
     maze = Maze()
-    lbl = Label(root, text=("W = " + str(maze.w) + ", H = " + str(maze.h)))
-    lbl.grid()
-    canvas = Canvas()
     data = maze.data
-    width = root.winfo_width()
-    height = root.winfo_height()
+    width = root.winfo_screenwidth()
+    height = root.winfo_screenheight()
+    print("Width: " + str(width) + ", height " + str(height))
+    canvas = Canvas(root, bg="white", height=height, width=width)
     wmap = round(width / tileSize)
     hmap = round(height / tileSize)
     center = [round(width / 2), round(height / 2)]
-    bot = [center - (maze.w / 2 * tileSize), center - (maze.h / 2 * tileSize)]
+    bot = [center[0] - (maze.w * .8 / 2 * tileSize), center[1] + (maze.h * .8 / 2 * tileSize)]
+    print("Center: " + str(center))
+    print("Bot: " + str(bot))
+
     for i in range(0, maze.w):
         for j in range(0, maze.h):
-            corn = [bot[0] + (i * tileSize), bot[1] + (j * tileSize)]
-            if(data[i][j])
+            # data[i][j].setWalls([1, 1, 1, 1])
+            # print("Tile [" + str(i) + ", " + str(j) + "] Walls: " + str(data[i][j].walls))
+            corn = [bot[0] + (j * tileSize), bot[1] - (i * tileSize)]
+
+            if data[i][j].getNorth():
+                canvas.create_line(corn[0], corn[1] - tileSize, corn[0] + tileSize, corn[1] - tileSize, fill="black")
+            if data[i][j].getSouth():
+                canvas.create_line(corn[0], corn[1], corn[0] + tileSize, corn[1], fill="black")
+            if data[i][j].getEast():
+                canvas.create_line(corn[0] + tileSize, corn[1], corn[0] + tileSize, corn[1] - tileSize, fill="black")
+            if data[i][j].getWest():
+                canvas.create_line(corn[0], corn[1], corn[0], corn[1] - tileSize, fill="black")
+            if labels:
+                canvas.create_text(corn[0] + tileSize / 2, corn[1] - tileSize / 2, text=(str(j) + ", " + str(i)), fill="black", font="Helvetica " + str(fontSize))
+    robot = Bot(maze, canvas, tileSize)
+    cube = canvas.create_rectangle(bot[0] + 5, bot[1] - 5, bot[0] + 30, bot[1] - 30, fill="black")
+    canvas.pack()
     root.mainloop()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
